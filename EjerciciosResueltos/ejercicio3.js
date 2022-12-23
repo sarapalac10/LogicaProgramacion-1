d1d669e762f39b1855502b8295f265ff532bb827
Sara Palacio <saritapz01@hotmail.com>
2022-12-22 20:11:33 -0500

    Solve for problem #3

---
+++ EjerciciosResueltos/ejercicio3.js
@@ -0,0 +1,35 @@
+function countingWords(phrase, word) {
+    let originalWord = word;
+    let originalPhrase = phrase
+      .toLowerCase()
+      .replace(/[,!-?]/gi, "")
+      .split(" ");
+    let counterWord = 0;
+  
+    if (originalPhrase.includes(originalWord)) {
+      counterWord++;
+      let mapWord = {};
+  
+      for (let word of originalPhrase) {
+        if (mapWord[word]) {
+          mapWord[word]++;
+        } else {
+          mapWord[word] = 1;
+        }
+      }
+      counterWord = mapWord[originalWord];
+      if(counterWord>1){
+        console.log(`The word ${originalWord} appears ${counterWord} times in the phrase`)
+      }else if(counterWord=1){
+        console.log(`The word ${originalWord} appears just one time in the phrase`)
+      }
+    } 
+    
+    else {
+      counterWord = 0;
+      console.log(`The word ${originalWord} isn't in the phrase.`)
+    }
+    return counterWord;
+  }
+  
+  countingWords("HOOla gato gato, soy un gato con gATo!", "gato")
--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-function palindromeWord(word) {
-  let original = word;
-  let revWord = word.split('').reverse().join('');
+function countingWords(phrase, word) {
+  let originalWord = word;
+  let originalPhrase = phrase
+    .toLowerCase()
+    .replace(/[,!-?]/gi, "")
+    .split(" ");
+  let counterWord = 0;
 
-  if(original === revWord){
-    console.log(`True: ${word} is the same as ${revWord}`);
-  }else{
-    console.log(`False: ${word} is different from ${revWord}`);
+  if (originalPhrase.includes(originalWord)) {
+    counterWord++;
+    let mapWord = {};
+
+    for (let word of originalPhrase) {
+      if (mapWord[word]) {
+        mapWord[word]++;
+      } else {
+        mapWord[word] = 1;
+      }
+    }
+    counterWord = mapWord[originalWord];
+    if(counterWord>1){
+      console.log(`The word ${originalWord} appears ${counterWord} times in the phrase`)
+    }else if(counterWord=1){
+      console.log(`The word ${originalWord} appears just one time in the phrase`)
+    }
+  } 
+  
+  else {
+    counterWord = 0;
+    console.log(`The word ${originalWord} isn't in the phrase.`)
   }
+  return counterWord;
 }
 
-palindromeWord('anitalavalatina');
+countingWords("Buenas noches, esto es una prueba de prueba prueba prueba", "prueba")
